42272e8dba747fd98fef7ce667ca4509932afff2
Yivlx <vuxdev@gmail.com>
2018-07-23 18:18:37 -0400

    Null check in constructor

--- LavalinkManagerConfig.cs
+++ LavalinkManagerConfig.cs
@@ -1,9 +1,18 @@
-﻿using Discord;
+﻿using System;
+using Discord;
 
 namespace SharpLink
 {
     public class LavalinkManagerConfig
     {
+        public LavalinkManagerConfig()
+        {
+            if (string.IsNullOrWhiteSpace(WebSocketHost))
+                throw new ArgumentNullException(nameof(WebSocketHost));
+            if (string.IsNullOrWhiteSpace(RESTHost))
+                throw new ArgumentNullException(nameof(RESTHost));
+        }
+
         public string WebSocketHost = "0.0.0.0";
         public ushort WebSocketPort = 80;
         public string RESTHost = "0.0.0.0";
