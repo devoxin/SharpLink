3204a9eabdd66dc4eaa6950d40d695231b48253f
Yivlx <vuxdev@gmail.com>
2018-07-23 18:11:41 -0400

    Cleanup

--- LavalinkTrack.cs
+++ LavalinkTrack.cs
@@ -17,24 +17,25 @@ namespace SharpLink
 
         internal LavalinkTrack(JToken jsonTrack)
         {
-            TrackId = (string)jsonTrack["track"];
-            Identifier = (string)jsonTrack["info"]["identifier"];
-            IsSeekable = (bool)jsonTrack["info"]["isSeekable"];
-            Author = (string)jsonTrack["info"]["author"];
-            IsStream = (bool)jsonTrack["info"]["isStream"];
-            Length = (IsStream ? TimeSpan.MaxValue : TimeSpan.FromMilliseconds((ulong)jsonTrack["info"]["length"]));
-            Position = (ulong)jsonTrack["info"]["position"];
-            Title = (string)jsonTrack["info"]["title"];
-            Url = (string)jsonTrack["info"]["uri"];
+            TrackId = (string) jsonTrack["track"];
+            Identifier = (string) jsonTrack["info"]["identifier"];
+            IsSeekable = (bool) jsonTrack["info"]["isSeekable"];
+            Author = (string) jsonTrack["info"]["author"];
+            IsStream = (bool) jsonTrack["info"]["isStream"];
+            Length = IsStream ? TimeSpan.MaxValue : TimeSpan.FromMilliseconds((ulong) jsonTrack["info"]["length"]);
+            Position = (ulong) jsonTrack["info"]["position"];
+            Title = (string) jsonTrack["info"]["title"];
+            Url = (string) jsonTrack["info"]["uri"];
         }
 
         // This constructor is used from the TrackId parser and will contain less info
-        internal LavalinkTrack(string trackId, string title, string author, ulong length, string identifer, bool isStream, string url, ulong position)
+        internal LavalinkTrack(string trackId, string title, string author, ulong length, string identifer,
+            bool isStream, string url, ulong position)
         {
             TrackId = trackId;
             Title = title;
             Author = author;
-            Length = (IsStream ? TimeSpan.MaxValue : TimeSpan.FromMilliseconds(length));
+            Length = IsStream ? TimeSpan.MaxValue : TimeSpan.FromMilliseconds(length);
             Identifier = identifer;
             IsStream = isStream;
             Url = url;
