adf50dee08adfb40ac2b7b9c4dfa2a0b0f1f154d
NovusTheory <novustheory@modulobot.xyz>
2018-09-14 22:15:23 -0500

    Incorrect default config
    0.0.0.0 -> 127.0.0.1 (You can't connect to 0.0.0.0)
    Resolves #21

--- SharpLink/LavalinkManagerConfig.cs
+++ SharpLink/LavalinkManagerConfig.cs
@@ -5,9 +5,9 @@ namespace SharpLink
     public class LavalinkManagerConfig
     {
         // Default config follows the application.yml.example in the Lavalink GitHub located at https://github.com/Frederikam/Lavalink/blob/master/LavalinkServer/application.yml.example
-        public string WebSocketHost = "0.0.0.0";
+        public string WebSocketHost = "127.0.0.1";
         public ushort WebSocketPort = 80;
-        public string RESTHost = "0.0.0.0";
+        public string RESTHost = "127.0.0.1";
         public ushort RESTPort = 2333;
         public string Authorization = "youshallnotpass";
         public int TotalShards = 1;
