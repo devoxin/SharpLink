59efdcfa15a78fb1511ce09bff49c30ae9dd27ba
James Daniel Cunningham <james@tegature.com>
2018-10-23 18:19:17 -0400

    Fixes for when tracks are replaced, updates to Lavalink config, and fixes for when non-sharded clients get disconnected. (#25)
    * Fix for replaced tracks on TrackEnd event.
    
    * Exception fixes and various cleanup
    
    - Fixed exception with getting a player value from the dictionary when it doesn't exist by replacing it with TryGetValue instead.
    
    - Fixed exception and player lock not being released when modifying the players dictionary during a non-sharded client disconnect.
    
    - Changed a lot of ContainsKey to TryGetValue since it's faster than doing two lookups on the dictionary.
    
    - Stopped removing the player after disconnect inside of this class since we already remove it when we attempt to use LavalinkPlayer.DisconnectAsync.
    
    - Updated RemovePlayerAsync to stop checking if the dictionary contains the guild ID because using remove within the dictionary already does that.
    
    * Updates config and removes session id functions.
    
    - Combined REST and Web Server settings to be inline with latest Lavalink version in config.
    
    - Removed session id functions as UserVoiceStateUpdated isn't called if the client is already connected to the voice channel. (Usually when the client doesn't get to properly disconnect. ie. Client.Disconnect or bot / app is exited)
    
    - Removed voice channel disconnect from JoinAsync as it won't actually disconnect us from the voice channel, even if it did, it'd be cancelling persistence and forcing a new connection which isn't great either.
    
    * Lavalink config changes
    
    * Updated config to support previous versions
    
    * Properly get voice session id upon reconnect
    
    * Readds small comment
    
    Missed this when I reverted my previous changes, whoops!
    
    * Put comment in wrong place =/
    
    * Only disconnect players that are connected
    
    - Makes sure that we actually have the session id set when we get disconnected from the Discord gateway and try to join a new player to the channel while the previous session is still ongoing.

--- SharpLink/LavalinkManagerConfig.cs
+++ SharpLink/LavalinkManagerConfig.cs
@@ -6,7 +6,7 @@ namespace SharpLink
     {
         // Default config follows the application.yml.example in the Lavalink GitHub located at https://github.com/Frederikam/Lavalink/blob/master/LavalinkServer/application.yml.example
         public string WebSocketHost = "127.0.0.1";
-        public ushort WebSocketPort = 80;
+        public ushort WebSocketPort = 2333;
         public string RESTHost = "127.0.0.1";
         public ushort RESTPort = 2333;
         public string Authorization = "youshallnotpass";
