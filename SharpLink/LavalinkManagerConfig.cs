8488974ea15628499c696f93d23639f66afe877c
Freya Arbjerg <freya@arbjerg.dev>
2021-04-18 10:52:10 +0200

    Fix old username

--- SharpLink/LavalinkManagerConfig.cs
+++ SharpLink/LavalinkManagerConfig.cs
@@ -4,7 +4,7 @@ namespace SharpLink
 {
     public class LavalinkManagerConfig
     {
-        // Default config follows the application.yml.example in the Lavalink GitHub located at https://github.com/Frederikam/Lavalink/blob/master/LavalinkServer/application.yml.example
+        // Default config follows the application.yml.example in the Lavalink GitHub located at https://github.com/freyacodes/Lavalink/blob/master/LavalinkServer/application.yml.example
         public string WebSocketHost = "127.0.0.1";
         public ushort WebSocketPort = 2333;
         public string RESTHost = "127.0.0.1";
